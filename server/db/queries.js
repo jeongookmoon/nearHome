ff535367246e400f0d90df3e9f1092b136239d26
Jeong Ook Moon <moonjeongook@gmail.com>
2019-06-17 23:52:26 -0700

    finished implementing search backend api, updated readme, added search function(display on console) on frontend

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,6 +1,11 @@
-module.exports = Object.freeze({
-  CREATE_DB: "CREATE DATABASE IF NOT EXISTS nearhome",
-  USE_DB: "USE nearhome",
+const mysql = require("mysql")
+const config = require("./configLocal")
+const connection = mysql.createConnection(config.configLocal)
+const DATABASE = "nearhome"
+
+module.exports = ({
+  CREATE_DB: `CREATE DATABASE IF NOT EXISTS ${DATABASE}`,
+  USE_DB: `USE ${DATABASE}`,
   DROP_BOOKING: "DROP TABLE IF EXISTS booking",
   CREATE_BOOKING: "CREATE TABLE IF NOT EXISTS booking "
     + "(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, "
@@ -10,9 +15,28 @@ module.exports = Object.freeze({
     + "user_id VARCHAR(255) NOT NULL)",
   DROP_USER: "DROP TABLE IF EXISTS user",
   CREATE_USER: "CREATE TABLE IF NOT EXISTS user "
-    + "(id VARCHAR(255) NOT NULL, "
+    + "(id VARCHAR(255) NOT NULL UNIQUE, "
     + "name VARCHAR(255) NOT NULL)",
-  INSERT_BOOKING: "INSERT INTO booking(id, property_id, property_name, city, user_id)" 
-  + "VALUES(null, ?, ?, ?, ?)",
-  SEARCH_BOOKINGS_BY_USER_ID: "SELECT * FROM booking AS B1 where B1.user_id = ?"
+  INSERT_BOOKING: `INSERT INTO ${DATABASE}.booking(id, property_id, property_name, city, user_id)`
+    + `VALUES(null, ?, ?, ?, ?)`,
+  INSERT_USER: `INSERT IGNORE INTO ${DATABASE}.user(id, name)`
+    + `VALUES(?, ?)`,
+  SEARCH_BOOKINGS_BY_USER_ID: `SELECT * FROM ${DATABASE}.booking AS B1 where B1.user_id = ? `,
+  SEARCH_BOOKINGS_BY_PROPERTY_ID: `SELECT B1.id AS id, B1.property_id AS property_id, `
+    + `B1.property_name AS property_name, B1.city AS city, `
+    + `JSON_OBJECT('id', B1.user_id, 'name', U1.name) AS user `
+    + `FROM ${DATABASE}.booking AS B1 INNER JOIN ${DATABASE}.user as U1 `
+    + `on B1.user_id = U1.id where B1.property_id = ?`,
+  SEARCH_USER_BY_USER_ID: `SELECT * FROM ${DATABASE}.user AS U1 where U1.id = ?`,
+  run: (query) => {
+    return new Promise((resolve, reject) => {
+      connection.query(query, (error, results) => {
+        if (error)
+          reject(error)
+        else {
+          resolve(results)
+        }
+      })
+    })
+  }
 })
